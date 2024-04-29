6ac269407cd448ec13f906c65e63e0d430e62550
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-04-29 16:07:49 +0200

    Created SpringBoot Project (JDK21) and mapped entities

---
+++ TaskLynx-SpringBoot/src/main/java/edu/danielalmazan/tasklynxspringboot/models/entity/Trabajador.java
@@ -0,0 +1,119 @@
+package edu.danielalmazan.tasklynxspringboot.models.entity;
+
+import jakarta.persistence.*;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "trabajador")
+public class Trabajador {
+    @Id
+    @Column(name = "id_trabajador", nullable = false, length = 5)
+    private String idTrabajador;
+
+    @Column(name = "dni", nullable = false, length = 9)
+    private String dni;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "apellidos", nullable = false, length = 100)
+    private String apellidos;
+
+    @Column(name = "especialidad", nullable = false, length = 50)
+    private String especialidad;
+
+    @Column(name = "\"contraseña\"", nullable = false, length = 50)
+    private String contraseña;
+
+    @Column(name = "email", nullable = false, length = 150)
+    private String email;
+
+    @OneToMany(mappedBy = "idTrabajador")
+    private Set<Trabajo> trabajos = new LinkedHashSet<>();
+
+    public Trabajador() {
+    }
+
+    public Trabajador(
+            String dni,
+            String nombre,
+            String apellidos,
+            String email,
+            String contraseña,
+            String especialidad
+    ) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.especialidad = especialidad;
+        this.contraseña = contraseña;
+        this.email = email;
+    }
+
+    public String getIdTrabajador() {
+        return idTrabajador;
+    }
+
+    public void setIdTrabajador(String idTrabajador) {
+        this.idTrabajador = idTrabajador;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Trabajo> getTrabajos() {
+        return trabajos;
+    }
+
+    public void setTrabajos(Set<Trabajo> trabajos) {
+        this.trabajos = trabajos;
+    }
+
+}
