415c5b681a97d3a386a5eb26179e198c8452f877
Aitor Moreno <119342012+LtVish@users.noreply.github.com>
2024-05-05 13:32:13 +0200

    Employees CRUD and views 100% functional.

--- src/main/java/edu/tasklynx/tasklynxspringboot/controllers/TrabajadoresViewController.java
+++ src/main/java/edu/tasklynx/tasklynxspringboot/controllers/TrabajadoresViewController.java
@@ -2,10 +2,8 @@ package edu.tasklynx.tasklynxspringboot.controllers;
 
 import edu.tasklynx.tasklynxspringboot.models.entity.Trabajador;
 import edu.tasklynx.tasklynxspringboot.models.services.TrabajadorServices;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,54 +17,48 @@ public class TrabajadoresViewController {
     @Autowired
     TrabajadorServices trabajadorServices = new TrabajadorServices();
 
+    // Enlaza a la página principal de trabajadores
     @GetMapping("")
     public String indexTrabajadores(Model model) {
         model.addAttribute("titulo", "Trabajadores");
-        return "trabajadores";
+        return "trabajadores/indexTrabajadores";
     }
 
-    // This method lists all workers
+    // Lista todos los trabajadores
     @GetMapping("/listar")
     public String listarTrabajadores(Model model) {
         model.addAttribute("titulo", "Listado de trabajadores");
         model.addAttribute("trabajadores", trabajadorServices.findAll());
-        return "trabajadoresListar";
+        return "trabajadores/trabajadoresListar";
     }
 
-    // This method shows the details of a worker
+    // Muestra los detalles de un trabajador
     @GetMapping("/{id}")
     public String verTrabajador(Model model, @PathVariable String id) {
-        model.addAttribute("titulo", "Trabajador");
+        model.addAttribute("titulo", "Trabajador " + id);
         model.addAttribute("trabajador", trabajadorServices.findById(id));
-        return "trabajadorVer";
+        return "trabajadores/trabajadorVer";
     }
 
-    // This method links to the form to add a new worker
+    // Enlaza al formulario para añadir un trabajador
     @GetMapping("/nuevo")
     public String anyadirTrabajador(Model model) {
+        model.addAttribute("titulo", "Añadir trabajador");
         addAtributes(model);
         model.addAttribute("trabajador", new Trabajador());
-        return "trabajadoresForm";
+        return "trabajadores/trabajadoresCrear";
     }
 
-    // This method links to the form to edit a worker
+    // Enlaza al formulario para editar un trabajador
     @GetMapping("/editar")
     public String editarTrabajador(Model model, @RequestParam String idTrabajador) {
+        model.addAttribute("titulo", "Editar trabajador");
         addAtributes(model);
         model.addAttribute("trabajador", trabajadorServices.findById(idTrabajador));
-        return "trabajadoresForm";
+        return "trabajadores/trabajadoresEditar";
     }
 
-    @RequestMapping(value="/update", method=RequestMethod.POST)
-    public ModelAndView updateOrCreate(@Valid Trabajador trabajador, BindingResult result, Model mod, HttpServletRequest request) {
-        if ("put".equals(request.getParameter("_method"))) {
-            return edit(trabajador, result, mod);
-        } else {
-            return create(trabajador, result, mod);
-        }
-    }
-
-    // This method creates a new worker
+    // Crea un trabajador
     @RequestMapping(value="/create", method=RequestMethod.POST)
     public ModelAndView create(@Valid Trabajador trabajador, BindingResult result, Model mod) {
         ModelAndView model = new ModelAndView();
@@ -92,7 +84,7 @@ public class TrabajadoresViewController {
                 model.addObject("mensaje", "El trabajador ya existe.");
             }
         } else {
-            model.setViewName("trabajadoresForm");
+            model.setViewName("trabajadores/trabajadoresCrear");
             model.addObject("titulo", "Error");
             model.addObject("mensaje", "Ha habido un error al añadir el trabajador.");
         }
@@ -101,7 +93,7 @@ public class TrabajadoresViewController {
         return model;
     }
 
-    // This method updates a worker
+    // Edita un trabajador
     @RequestMapping(value="/edit", method=RequestMethod.PUT)
     public ModelAndView edit(@Valid Trabajador trabajador, BindingResult result, Model mod) {
         ModelAndView model = new ModelAndView();
@@ -113,7 +105,7 @@ public class TrabajadoresViewController {
             model.addObject("titulo", "Trabajador actualizado");
             model.addObject("mensaje", "El trabajador ha sido actualizado correctamente.");
         } else {
-            model.setViewName("trabajadoresForm");
+            model.setViewName("trabajadores/trabajadoresEditar");
             model.addObject("titulo", "Error");
             model.addObject("mensaje", "Ha habido un error al actualizar el trabajador.");
         }
@@ -122,6 +114,7 @@ public class TrabajadoresViewController {
         return model;
     }
 
+    // Elimina un trabajador
     @RequestMapping("/delete")
     public ModelAndView delete(@RequestParam String idTrabajador) {
         ModelAndView model = new ModelAndView();
@@ -132,8 +125,8 @@ public class TrabajadoresViewController {
         return model;
     }
 
+    // Método auxiliar para añadir atributos a la vista (formularios añadir y editar)
     private void addAtributes(Model mod) {
-        mod.addAttribute("titulo", "Añadir trabajador");
         mod.addAttribute("id", "ID:");
         mod.addAttribute("dni", "DNI:");
         mod.addAttribute("nombre", "Nombre:");
